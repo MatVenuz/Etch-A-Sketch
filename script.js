081b8c5fb8ba2b0a7115459dd0f95c992d4f28d0
MatYGVZ <jesusprogrammignacc17@gmail.com>
2022-05-01 20:48:44 -0400

    rainbow button

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function makeGrid(size) {
     for (c = 0; c < (size * size); c++) {
       let cell = document.createElement("div");
       container.appendChild(cell).className = "grid-item";
-      cell.addEventListener('mouseover', () => {
+      cell.addEventListener("mouseover", () => {
           cell.style.background = "black"
       })
     };
@@ -32,3 +32,17 @@ buttonSize.addEventListener("click", () =>  {
         makeGrid(10)
     }
 });
+
+// rainbow button
+rainbowButton.addEventListener("click", () => {
+    const gridItem = document.querySelectorAll(".grid-item");
+    gridItem.forEach(element => element.addEventListener("mouseover", () => {
+        randomColor(element)
+    }))
+})
+
+// rainbow generator
+function randomColor(gItem) {
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    gItem.style.background = "#" + randomColor
+}
